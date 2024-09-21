8be40e3e8a15dfd8b0e1f30bbcee3e4e1871807f
goodlucktokdy <148170828+goodlucktokdy@users.noreply.github.com>
2024-09-21 16:20:01 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ MySQL/프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,12 +1,30 @@
-WITH RECURSIVE CTE AS
-(SELECT ID, PARENT_ID, 1 AS DEPTH
-FROM ECOLI_DATA
-WHERE PARENT_ID IS NULL
-UNION ALL
-SELECT A.ID, A.PARENT_ID, C.DEPTH + 1
-FROM ECOLI_DATA AS A
-JOIN CTE AS C
-ON C.ID = A.PARENT_ID)
-SELECT ID FROM CTE
-WHERE DEPTH = 3
-ORDER BY ID ASC
+with recursive cte as (
+    select
+        id,
+        parent_id,
+        1 as generation
+    from
+        ECOLI_DATA
+    where 
+        parent_id is null
+    
+    union all
+    
+    select
+        a.id,
+        a.parent_id,
+        cte.generation + 1
+    from
+        ECOLI_DATA a
+    join
+        cte 
+    on
+        a.parent_id = cte.id)
+        
+select id from cte
+where
+    cte.generation = 3
+order by
+    id
+
+    
