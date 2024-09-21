5ee550532584d38796caccb7f7c89c9cecb92b89
goodlucktokdy <148170828+goodlucktokdy@users.noreply.github.com>
2024-09-21 21:06:24 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ MySQL/프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,7 +1,28 @@
-WITH U AS (SELECT * FROM FIRST_HALF
-UNION ALL
-SELECT * FROM JULY)
-SELECT A.FLAVOR
-FROM
-(SELECT FLAVOR, SUM(TOTAL_ORDER) AS TOT FROM U
-       GROUP BY FLAVOR ORDER BY TOT DESC LIMIT 3) AS A
+with base as (
+select
+    flavor,
+    total_order
+from
+    first_half
+union all
+
+select
+    flavor,
+    total_order
+from
+    july
+)
+select
+    a.flavor
+from (
+    select
+       flavor,
+       sum(total_order) as tot_order
+    from
+        base
+    group by
+        flavor
+    order by
+        tot_order desc
+) a
+limit 3
