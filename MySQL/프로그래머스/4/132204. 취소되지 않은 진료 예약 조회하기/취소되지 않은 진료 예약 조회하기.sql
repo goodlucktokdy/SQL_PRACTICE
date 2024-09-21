ada36fb5c67e25ddbd034238850d4db89854b861
goodlucktokdy <148170828+goodlucktokdy@users.noreply.github.com>
2024-09-21 21:47:37 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ MySQL/프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,11 +1,40 @@
-SELECT B.APNT_NO, C.PT_NAME, B.PT_NO, B.MCDP_CD, A.DR_NAME, B.APNT_YMD
-FROM APPOINTMENT AS B
-LEFT JOIN
-DOCTOR AS A
-ON A.DR_ID = B.MDDR_ID
-INNER JOIN
-PATIENT AS C
-ON C.PT_NO = B.PT_NO
-WHERE B.MCDP_CD = 'CS' AND DATE_FORMAT(B.APNT_YMD,'%Y-%m-%d') = '2022-04-13'
-AND B.APNT_CNCL_YN != 'Y'
-ORDER BY B.APNT_YMD ASC
+with base as (
+select
+    a.pt_no,
+    a.pt_name,
+    b.apnt_ymd,
+    b.apnt_no,
+    b.mcdp_cd,
+    b.apnt_cncl_yn,
+    c.dr_name,
+    c.dr_id,
+    b.mddr_id
+from
+    appointment b
+left join
+    patient a
+on
+    b.pt_no = a.pt_no
+left join
+    doctor c
+on
+    c.dr_id = b.mddr_id
+)
+select
+    apnt_no,
+    pt_name,
+    pt_no,
+    mcdp_cd,
+    dr_name,
+    apnt_ymd
+from
+    base
+where
+    date(apnt_ymd) = '2022-04-13'
+and
+    mcdp_cd = 'CS'
+and
+    apnt_cncl_yn = 'N'
+order by
+    apnt_ymd
+ 
