e50f5ce2f1241b4d8c9a4c267f738a8a36d250d6
goodlucktokdy <148170828+goodlucktokdy@users.noreply.github.com>
2024-09-25 00:24:43 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ MySQL/프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,3 +1,46 @@
+# with base as (
+#     select
+#         a.car_id,
+#         a.car_type,
+#         a.daily_fee,
+#         b.history_id,
+#         b.start_date,
+#         b.end_date,
+#         date(b.end_date) - date(b.start_date) + 1 as duration
+#     from
+#         car_rental_company_car a
+#     join
+#         car_rental_company_rental_history b
+#     on 
+#         a.car_id = b.car_id
+#     where a.car_type = '트럭'
+# ), duration_table as (  
+#     select
+#         a.car_type,
+#         a.history_id,
+#         a.daily_fee,
+#         a.duration,
+#         case when 
+#             b.discount_rate is null then 0 else b.discount_rate end as discount_rate
+#     from
+#         base a
+#     left join
+#          CAR_RENTAL_COMPANY_DISCOUNT_PLAN b
+#     on
+#         a.car_type = b.car_type and
+#         (b.duration_type = case when 
+#             a.duration >= 90 then '90일 이상'
+#             when a.duration >= 30 then '30일 이상'
+#             when a.duration >= 7 then '7일 이상'
+#             else 0 end)
+# )
+# select
+#     history_id,
+#     round(daily_fee * duration * (1-0.01*discount_rate)) as fee
+# from 
+#     duration_table
+# order by
+#     fee desc, history_id desc
 with base as (
 select 
     b.history_id,
