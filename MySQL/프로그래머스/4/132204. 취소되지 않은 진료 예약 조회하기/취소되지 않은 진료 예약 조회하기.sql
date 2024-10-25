963bf192ef3086837c81b46139e470b5f01aa58c
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-10-25 15:52:35 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ MySQL/프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,24 +1,28 @@
 with base as (
-select
-    a.pt_no,
-    a.pt_name,
-    b.apnt_ymd,
-    b.apnt_no,
-    b.mcdp_cd,
-    b.apnt_cncl_yn,
-    c.dr_name,
-    c.dr_id,
-    b.mddr_id
-from
-    appointment b
-left join
-    patient a
-on
-    b.pt_no = a.pt_no
-left join
-    doctor c
-on
-    c.dr_id = b.mddr_id
+    select
+        a.apnt_ymd,
+        a.apnt_no,
+        a.pt_no,
+        a.mcdp_cd,
+        a.mddr_id,
+        c.pt_name,
+        b.dr_name
+    from 
+        appointment a
+    left join
+        doctor b
+    on
+        a.mddr_id = b.dr_id and a.mcdp_cd = b.mcdp_cd
+    left join
+        patient c
+    on
+        a.pt_no = c.pt_no
+    where
+        date(apnt_ymd) = '2022-04-13' 
+    and
+        a.MCDP_CD = 'CS'
+    and
+        apnt_cncl_yn = 'N'
 )
 select
     apnt_no,
@@ -27,14 +31,7 @@ select
     mcdp_cd,
     dr_name,
     apnt_ymd
-from
+from 
     base
-where
-    date(apnt_ymd) = '2022-04-13'
-and
-    mcdp_cd = 'CS'
-and
-    apnt_cncl_yn = 'N'
 order by
     apnt_ymd
- 
