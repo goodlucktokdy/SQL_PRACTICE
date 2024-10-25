734e831370484d5723449ffba6f073cc0fdb201c
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-10-25 14:34:02 +0900

    [level 4] Title: 5월 식품들의 총매출 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
+++ MySQL/프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
@@ -1,17 +1,16 @@
 select
-    a.product_id,
-    b.product_name,
-    sum(a.amount*b.price) as total_sales
-from
-    food_order a
-left join
-    food_product b
-on
+    b.product_id,
+    a.product_name,
+    sum(a.price * b.amount) as TOTAL_SALES
+from 
+    food_product a 
+left join 
+    food_order b
+on 
     a.product_id = b.product_id
 where
-    date(a.produce_date) between '2022-05-01' and '2022-05-31'
-    and b.price is not null
-group by
-    b.product_id,b.product_name
-order by
-    total_sales desc, product_id asc
+    b.produce_date between '2022-05-01' and '2022-05-31'
+group by 
+    1,2
+order by 
+    TOTAL_SALES desc, product_id asc
