95fddc595421254810adae39d226054d45e8f076
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-10-26 23:22:44 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ MySQL/프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,22 +1,23 @@
--- 코드를 입력하세요
 with base as (
-    SELECT
-        distinct
-        cart_id,
-        name,
-        case when 
-            name in ('Milk','Yogurt') then 1 else 0 end as temp
-    from 
-        cart_products
+    select
+        a.cart_id,
+        sum(case when a.name in ('Yogurt','Milk') then 1 else 0 end) over (partition by a.cart_id) as cnts,
+        a.name
+    from (
+        select
+            distinct
+            cart_id,
+            name
+        from 
+            cart_products
+    ) a   
 )
-select 
+select
+    distinct
     cart_id
 from 
-    base
-group by 
-    cart_id
-having
-    sum(temp) = 2
+    base 
+where
+    cnts = 2
 order by 
     cart_id
-
