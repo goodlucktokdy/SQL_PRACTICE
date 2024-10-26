b4e6f17176e99344384192b0e07b903c09d31a3c
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-10-26 12:09:42 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ MySQL/프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,48 +1,39 @@
-with base as ( 
-    select 
-        a.car_id,
-        a.car_type,
-        a.daily_fee,
-        b.history_id,
-        b.start_date,
-        b.end_date
-    from
-        car_rental_company_car a
-    join
-        car_rental_company_rental_history b
-    on
-        a.car_id = b.car_id 
-    where not exists
-    (select end_date from car_rental_company_rental_history b
-        where a.car_id = b.car_id and
-     (b.end_date >= '2022-11-01' and b.start_date <= '2022-11-30'))
-),duration_table as ( 
+with base as (
     select
-        distinct
+        a.history_id,
         a.car_id,
-        a.car_type,
-        a.daily_fee,
         a.start_date,
         a.end_date,
-        b.discount_rate,
-        b.duration_type
-    from
-        base a
-    join
-        (select car_type, duration_type, discount_rate
-         from car_rental_company_discount_plan
-        where duration_type = '30일 이상') b
+        timestampdiff(day,a.start_date,a.end_date) + 1 as days_diff,
+        b.daily_fee,
+        b.car_type,
+        c.duration_type,
+        c.discount_rate
+    from 
+        CAR_RENTAL_COMPANY_RENTAL_HISTORY a
+    left join
+        CAR_RENTAL_COMPANY_CAR b
+    on 
+        a.car_id = b.car_id
+    left join
+        CAR_RENTAL_COMPANY_DISCOUNT_PLAN c
     on
-        a.car_type = b.car_type  )      
+        b.car_type = c.car_type
+)
 select
-    distinct
     car_id,
     car_type,
-    round(30 * daily_fee * (1 - discount_rate*0.01)) as fee
-from
-    duration_table
+    round(daily_fee* 30*(1-0.01*discount_rate)) as fee
+from 
+    base
 where
-    30 * daily_fee * (1-discount_rate*0.01) >= 500000 and
-    30 * daily_fee * (1-discount_rate*0.01) < 2000000
-order by
+    car_id not in (select distinct car_id from base
+    where end_date > '2022-11-01')
+    and car_type in ('SUV','세단')
+    and duration_type = '30일 이상'
+group by 
+    car_id
+having 
+    fee >= 500000 and fee < 2000000
+order by 
     fee desc, car_type asc, car_id desc
