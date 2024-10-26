54546423f107ec7c28029042b40f76fa0ebaa616
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-10-26 13:36:55 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ MySQL/프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,12 +1,9 @@
--- 코드를 입력하세요
-with base as (
-SELECT
-   extract(year from date(sales_date)) as year,
-   extract(month from date(sales_date)) as month,
-   date_format(joined,'%Y-%m-%d') as joined,
-   b.user_id,
-    b.product_id,
-    b.sales_amount
+select
+    year(b.sales_date) as year,
+    month(b.sales_date) as month,
+    count(distinct b.user_id) as purchased_users,
+    round(count(distinct b.user_id)/(select count(distinct user_id) from user_info
+                                    where year(joined) = 2021),1) as purchased_ratio
 from
     user_info a
 join
@@ -14,17 +11,8 @@ join
 on
     a.user_id = b.user_id
 where
-    extract(year from joined) = '2021'
-)
-select
-    year,
-    month,
-    count(distinct user_id) as purchased_users,
-    round(count(distinct user_id)/(select count(distinct user_id) from user_info
-                                where joined between '2021-01-01' and '2021-12-31'),1) as purchased_ratio
-from
-    base
+    year(a.joined) = 2021
 group by
-    year,month
-order by
+    year, month
+order by 
     year, month
