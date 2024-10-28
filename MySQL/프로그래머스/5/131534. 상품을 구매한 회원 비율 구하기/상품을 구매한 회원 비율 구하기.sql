790407b142cf823cebb1979205e7d2b7ebac621e
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-10-28 21:57:26 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ MySQL/프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,20 +1,33 @@
-select 
-    year(b.sales_date) as year,
-    month(b.sales_date) as month,
-    count(distinct b.user_id) as purchased_customers,
-    round(count(distinct b.user_id)/(select count(distinct user_id) from user_info where
-     year(joined) = 2021),1) as purchased_ratio
-from
-    user_info a
-left join 
-    online_sale b
-on 
-    a.user_id = b.user_id
+with base as (
+    select
+        year(b.sales_date) as year,
+        month(b.sales_date) as month,
+        count(distinct case when year(a.joined) = 2021 then b.user_id end) as purchased_users     from 
+        user_info a
+    left join
+        online_sale b
+    on 
+        a.user_id = b.user_id
+    group by 
+        1,2
+)
+, mid_tb as (
+    select
+        year,
+        month,
+        purchased_users,
+        (select count(distinct user_id) from user_info where year(joined) = 2021) as joined_cnt
+    from 
+        base
+)
+select
+    year,
+    month,
+    purchased_users,
+    round(purchased_users/joined_cnt,1) as purchased_ratio
+from 
+    mid_tb
 where
-    year(a.joined) = 2021 and year(b.sales_date) is not null and month(b.sales_date) is not null
-group by 
-    1,2
+    year is not null and month is not null
 order by 
-    year asc,
-    month asc 
-    
+    year asc, month asc
