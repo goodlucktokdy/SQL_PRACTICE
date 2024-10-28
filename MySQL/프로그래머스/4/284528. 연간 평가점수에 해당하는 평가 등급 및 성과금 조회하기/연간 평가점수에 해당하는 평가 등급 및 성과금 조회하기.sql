f9779951ea5cda48176391ef1ec921104afdb3d9
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-10-28 18:33:59 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ MySQL/프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,23 +1,33 @@
+with base as (
 select
-    a.emp_no,
-    b.emp_name,
-    case when 
-        avg(a.score) >= 96 then 'S' 
-        when avg(a.score) >= 90 then 'A'
-        when avg(a.score) >= 80 then 'B'
-        else 'C' end as grade,
-    case when 
-        avg(a.score) >= 96 then b.sal * 0.2
-        when avg(a.score) >= 90 then b.sal * 0.15
-        when avg(a.score) >= 80 then b.sal * 0.1
-        else 0 end as bonus
+    b.emp_no,
+    a.emp_name,
+    a.sal,
+    avg(b.score) as score
 from 
-    hr_grade a
+    hr_employees a
 left join
-    hr_employees b
+    hr_grade  b
 on 
     a.emp_no = b.emp_no
 group by 
-    a.emp_no,b.emp_name
+    1,2,3
+)
+select
+    emp_no,
+    emp_name,
+    case when
+        score >= 96 then 'S'
+        when score >= 90 then 'A'
+        when score >= 80 then 'B'
+        else 'C' end as grade,
+    case when
+        score >= 96 then sal * 0.2
+        when score >= 90 then sal * 0.15
+        when score >= 80 then sal * 0.1
+        else 0 end as bonus
+from 
+    base
 order by 
-    a.emp_no
+    emp_no asc
+        
