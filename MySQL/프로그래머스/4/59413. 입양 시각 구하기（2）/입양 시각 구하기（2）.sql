ee86018e0d617cd906c69194a4b0d0c4aefc497f
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-10-28 20:27:17 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ MySQL/프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,15 +1,17 @@
 with recursive cte as (
-    select 
+    select
         0 as hour
     union all
     select
         cte.hour + 1
     from 
         cte
-    where cte.hour < 23)
+    where
+        cte.hour < 23
+)
 select
     a.hour,
-    count(distinct b.animal_id) as count
+    count(animal_id) as count
 from 
     cte a
 left join
@@ -17,4 +19,6 @@ left join
 on 
     a.hour = hour(b.datetime)
 group by 
+    a.hour
+order by 
     a.hour
