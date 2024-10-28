37e576b6eb40d49c2d98de2542621e7bb7c264fa
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-10-28 17:33:32 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ MySQL/프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,15 +1,16 @@
--- 코드를 입력하세요
-SELECT
+select 
     b.animal_id,
     b.animal_type,
     b.name
 from 
     animal_ins a 
-left join
+join 
     animal_outs b
 on 
     a.animal_id = b.animal_id
 where
-    a.sex_upon_intake like '%Intact%' and
-    (b.sex_upon_outcome like '%Spayed%' or b.sex_upon_outcome like'%Neutered%')
-    
+    (a.sex_upon_intake like '%Intact%')
+    and (b.sex_upon_outcome like '%Spayed%'
+        or b.sex_upon_outcome like '%Neutered%')
+order by 
+    b.animal_id
