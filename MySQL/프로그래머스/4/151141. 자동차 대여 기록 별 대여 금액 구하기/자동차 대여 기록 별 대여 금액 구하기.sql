35762686583788725a61c881ba53301f336d37c0
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-10-30 08:42:46 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ MySQL/프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -4,42 +4,67 @@ with base as (
         a.car_id,
         a.start_date,
         a.end_date,
+        timestampdiff(day,a.start_date,a.end_date) + 1 as diff_of_day,
+        b.car_type,
         b.daily_fee,
-        b.car_type
+        c.duration_type,
+        c.discount_rate
     from 
-        car_rental_company_rental_history a
-    left join
-        car_rental_company_car b
-    on
+        CAR_RENTAL_COMPANY_RENTAL_HISTORY a
+    left join 
+        CAR_RENTAL_COMPANY_CAR b
+    on 
         a.car_id = b.car_id
+    left join 
+        CAR_RENTAL_COMPANY_DISCOUNT_PLAN c
+    on 
+        b.car_type = c.car_type
+    where
+        b.car_type = '트럭'
 )
-, discount_tb as (
+,diff_of_day_tb as (
     select
-        a.*,
-        timestampdiff(day,a.start_date,a.end_date) + 1 as period,
-        b.duration_type,
+        history_id,
+        car_id,
+        start_date,
+        end_date,
+        diff_of_day,
+        car_type,
+        daily_fee,
+        case when 
+            diff_of_day >= 90 then '90일 이상' 
+            when diff_of_day >= 30 then '30일 이상'
+            when diff_of_day >= 7 then '7일 이상'
+            else diff_of_day end as duration_type,
+        discount_rate
+    from 
+        base
+)
+, discount_joined_tb as (
+    select
+        a.history_id,
+        a.car_id,
+        a.start_date,
+        a.end_date,
+        a.diff_of_day,
+        a.car_type,
+        a.daily_fee,
+        a.duration_type,
         b.discount_rate
     from 
-        base a
-    left join 
+        diff_of_day_tb a 
+    left join
         car_rental_company_discount_plan b
     on 
-        a.car_type = b.car_type and 
-        b.duration_type = 
-        case when timestampdiff(day,a.start_date,a.end_date) + 1 >= 90 then '90일 이상'
-        when timestampdiff(day,a.start_date,a.end_date) + 1 >= 30 then '30일 이상'
-        when timestampdiff(day,a.start_date,a.end_date) + 1 >= 7 then '7일 이상'
-        else null end
-    where
-        a.car_type = '트럭'
+        a.duration_type = b.duration_type and a.car_type = b.car_type
 )
 select
+    distinct
     history_id,
     case when
-        duration_type is not null 
-        then round(period * daily_fee * (1-0.01*discount_rate))
-        else round(period * daily_fee) end as fee
+        discount_rate is not null then round(diff_of_day * (1-0.01*discount_rate) * daily_fee)
+        else round(diff_of_day * daily_fee) end as fee
 from 
-    discount_tb
+    discount_joined_tb
 order by 
     fee desc, history_id desc
