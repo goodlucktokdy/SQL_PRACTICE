0463a4a98fe61afc82a332462f4b218d86bee6d9
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-10-30 07:23:35 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ MySQL/프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,21 +1,35 @@
+with base as (
+    select
+        a.apnt_no,
+        b.pt_name,
+        a.pt_no,
+        a.mcdp_cd,
+        a.mddr_id,
+        c.dr_name,
+        a.apnt_ymd,
+        a.apnt_cncl_yn
+    from 
+        appointment a 
+    left join 
+        patient b 
+    on
+        a.pt_no = b.pt_no
+    left join 
+        doctor c
+    on
+        a.mddr_id = c.dr_id and a.mcdp_cd = c.mcdp_cd
+    where
+        date(a.apnt_ymd) = '2022-04-13' and a.mcdp_cd = 'CS'
+        and a.apnt_cncl_yn = 'N'
+    order by 
+        a.apnt_ymd asc
+)
 select
-    a.apnt_no,
-    b.pt_name,
-    a.pt_no,
-    a.mcdp_cd,
-    c.dr_name,
-    a.apnt_ymd
+    apnt_no,
+    pt_name,
+    pt_no,
+    mcdp_cd,
+    dr_name,
+    apnt_ymd
 from 
-    appointment a
-left join 
-    patient b
-on 
-    a.pt_no = b.pt_no
-left join 
-    doctor c
-on 
-    a.mddr_id = c.dr_id and a.mcdp_cd = c.mcdp_cd
-where
-    date(a.apnt_ymd) = '2022-04-13' and a.mcdp_cd = 'CS' and a.apnt_cncl_ymd is null
-order by 
-    a.apnt_ymd 
+    base 
