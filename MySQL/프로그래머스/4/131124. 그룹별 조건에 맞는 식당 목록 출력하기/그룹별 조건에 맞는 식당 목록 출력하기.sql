a66f981cbb7613ac1ddbaded1e5eaf1b3d9c36b7
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-11-17 18:22:29 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ MySQL/프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,38 +1,43 @@
+-- 코드를 입력하세요
 with base as (
-    select
+    SELECT
         distinct
-        a.member_id,
+        b.member_id,
         b.member_name,
-        a.rest_id,
         a.review_id,
         a.review_text,
-        a.review_date
+        date(a.review_date) as review_date
     from 
         rest_review a
-    left join 
+    join 
         member_profile b
     on 
         a.member_id = b.member_id
-),
-cnts_tb as (
-    select
-        member_id,
-        member_name,
-        review_text,
-        review_date,
-        count(review_id) over (partition by member_id) as cnts
-    from 
-        base
-    group by 
-        1,2,3,4
 )
-select
+,review_cnt_ranks_per_user as (
+    select
+        a.member_name,
+        a.review_text,
+        a.review_date,
+        dense_rank() over (order by a.review_cnt_per_user desc) as ranks
+    from (
+        select
+            member_name,
+            review_text,
+            review_date,
+            count(review_id) over (partition by member_id) as review_cnt_per_user
+        from 
+            base
+    ) a
+)
+select 
+    distinct
     member_name,
     review_text,
     date_format(review_date,'%Y-%m-%d') as review_date
 from 
-    cnts_tb
+    review_cnt_ranks_per_user
 where
-    cnts = (select max(cnts) from cnts_tb)
+    ranks = 1
 order by 
     review_date, review_text
