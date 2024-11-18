86b84ae862d8799eda8b9d0ca8739b872641ced7
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-11-18 19:13:08 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ MySQL/프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,34 +1,40 @@
--- 코드를 입력하세요
 with base as (
-    SELECT
+    select 
+        a.review_id,
         a.rest_id,
-        a.rest_name,
-        a.food_type,
-        a.favorites,
-        a.address,
-        b.review_score
+        a.review_score as score, 
+        b.rest_name,
+        b.food_type,
+        b.favorites,
+        b.address
     from 
-        rest_info a
+        rest_review a
     left join 
-        rest_review b 
+        rest_info b
     on 
         a.rest_id = b.rest_id
     where
-        substring(a.address,1,5) like '%서울%'
+        substring(b.address,1,5) like '%서울%'
 )
 select 
-   rest_id,
-   rest_name,
-   food_type,
-   favorites,
-   address, 
-   round(avg(review_score),2) as score   
-from 
-    base
+    a.rest_id,
+    a.rest_name,
+    a.food_type,
+    a.favorites,
+    a.address,
+    round(avg(a.score),2) as score
+from (
+    select 
+        rest_id,
+        rest_name,
+        food_type,
+        favorites,
+        address,
+        score
+    from 
+        base
+) a
 group by 
-    1,2,3,4,5
-having
-    score is not null
+    1,2,3,4
 order by 
     score desc, favorites desc
-    
