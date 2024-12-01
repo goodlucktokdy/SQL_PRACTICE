3ecc1eb44aa7209ac6451323496e0278623385d3
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-12-01 22:38:46 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -0,0 +1,22 @@
+with base as  (
+    select
+        food_type,
+        max(favorites) as max_favorites
+    from 
+        rest_info
+    group by 
+        food_type
+)
+select 
+    a.food_type,
+    a.rest_id,
+    a.rest_name,
+    a.favorites
+from 
+    rest_info a
+inner join
+    base b
+on
+    a.favorites = b.max_favorites and a.food_type = b.food_type
+order by 
+    a.food_type desc
