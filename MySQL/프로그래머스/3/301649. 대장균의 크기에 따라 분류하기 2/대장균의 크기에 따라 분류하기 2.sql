9f147700c04ba30f49b4b1fe25b51d424461f22d
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-12-02 21:01:32 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ MySQL/프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,15 +1,15 @@
-select 
+select
     a.id,
-    case when a.ranks <= 0.25 then 'CRITICAL'
-    when a.ranks <= 0.5 then 'HIGH'
-    when a.ranks <= 0.75 then 'MEDIUM'
-    else 'LOW' end as colony_name
+    case when a.percent <= 0.25 then 'CRITICAL' 
+        when a.percent <= 0.50 then 'HIGH'
+        when a.percent <= 0.75 then 'MEDIUM'
+        else 'LOW' end as colony_name
 from (
-select 
-    id, 
-    percent_rank() over (order by size_of_colony desc) as ranks
-from 
-    ecoli_data
+    select
+        id,
+        percent_rank() over (order by size_of_colony desc) as percent
+    from 
+        ecoli_data
 ) a
-order by
-    id asc
+order by 
+    a.id asc
