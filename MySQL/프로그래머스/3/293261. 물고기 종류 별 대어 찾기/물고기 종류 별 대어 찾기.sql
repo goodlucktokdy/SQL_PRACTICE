fbeff825b85551fad93654c9f8f9020dbc193463
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-12-02 21:25:16 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ MySQL/프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,26 +1,34 @@
-with base as (
-    select 
-        b.fish_type,
-        b.fish_name,
-        max(a.length) as max_length
+with max_fish_info as (
+    select
+        fish_type,
+        max(length) as max_length
     from 
-        fish_info a
-    left join
-        fish_name_info b
-    on 
-        a.fish_type = b.fish_type
+        fish_info
     group by 
-        b.fish_type, b.fish_name
+        fish_type
 )
-select 
-    b.id,
-    a.fish_name,
+, integrated_info as (
+    select
+        b.id,
+        a.fish_type,
+        b.length,
+        a.max_length
+    from 
+        max_fish_info a
+    inner join 
+        fish_info b 
+    on 
+        a.max_length = b.length and a.fish_type = b.fish_type
+)
+select
+    a.id,
+    b.fish_name,
     a.max_length as length
 from 
-    base a
-left join
-    fish_info b
-on
-    a.max_length = b.length and a.fish_type = b.fish_type
+    integrated_info a
+inner join 
+    fish_name_info b
+on 
+    a.fish_type = b.fish_type
 order by 
-    b.id asc
+    id asc
