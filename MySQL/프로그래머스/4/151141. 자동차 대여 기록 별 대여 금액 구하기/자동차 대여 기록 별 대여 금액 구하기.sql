e7cc0d4365d360b4a905b7209ab29d8fc8dc1b48
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-12-02 20:52:56 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ MySQL/프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,42 +1,47 @@
 with base as (
     select
+        a.history_id,
+        a.car_id,
+        b.car_type,
+        a.start_date,
+        a.end_date,
+        b.daily_fee,
+        timestampdiff(day,a.start_date, a.end_date) + 1 as diff_of_day
+    from 
+        car_rental_company_rental_history a
+    left join 
+        car_rental_company_car b
+    on 
+        a.car_id = b.car_id
+)
+, filter_discount_rate as (
+    select
+        a.history_id,
         a.car_id,
         a.car_type,
         a.daily_fee,
-        b.history_id,
-        b.start_date,
-        b.end_date,
-        c.duration_type,
-        c.discount_rate
+        a.diff_of_day,
+        b.duration_type,
+        b.discount_rate
     from 
-        car_rental_company_car a 
+        base a
     left join
-        car_rental_company_rental_history b 
-    on
-        a.car_id = b.car_id 
-    left join
-        car_rental_company_discount_plan c
-    on
-        a.car_type = c.car_type
-        and c.duration_type = case when timestampdiff(day,b.start_date,b.end_date) + 1 >= 90 then '90일 이상'
-        when timestampdiff(day,b.start_date,b.end_date) + 1 >= 30 then '30일 이상'
-        when timestampdiff(day,b.start_date,b.end_date) + 1 >= 7 then '7일 이상'
-        else null end
-    where 
-        a.car_type = '트럭'
+        car_rental_company_discount_plan b
+    on 
+        a.car_type = b.car_type and 
+        b.duration_type = case when a.diff_of_day >= 90 then '90일 이상'
+                                when a.diff_of_day >= 30 then '30일 이상'
+                                when a.diff_of_day >= 7 then '7일 이상'
+                                else null end
 )
-select 
-    a.history_id,
-    case when a.duration_type is not null then round(a.daily_fee * a.duration * 0.01 * (100-a.discount_rate)) else a.daily_fee * a.duration end as fee
-from (
-        select 
-            history_id,
-            daily_fee,
-            timestampdiff(day,start_date,end_date) + 1 as duration,
-            duration_type,
-            discount_rate
-        from 
-            base
-) a
+select
+    history_id,
+    case when duration_type is null then diff_of_day * daily_fee
+        else round(diff_of_day * daily_fee * (100 - discount_rate) * 0.01) end as fee
+from 
+    filter_discount_rate
+where 
+    car_type = '트럭'
 order by 
     fee desc, history_id desc
+    
