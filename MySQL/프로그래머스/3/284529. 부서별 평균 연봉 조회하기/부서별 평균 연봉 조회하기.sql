659fc633608af861af353007f6acf306c97037ca
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-12-02 21:28:05 +0900

    [level 3] Title: 부서별 평균 연봉 조회하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/3/284529. 부서별 평균 연봉 조회하기/부서별 평균 연봉 조회하기.sql
+++ MySQL/프로그래머스/3/284529. 부서별 평균 연봉 조회하기/부서별 평균 연봉 조회하기.sql
@@ -1,15 +1,21 @@
-select 
+with avg_sal_cte as (
+    select
+        dept_id,
+        avg(sal) as avg_sal
+    from 
+        hr_employees
+    group by 
+        dept_id
+)
+select
     a.dept_id,
     b.dept_name_en,
-    round(avg(a.sal),0) as avg_sal
+    round(a.avg_sal) as avg_sal
 from 
-    hr_employees a 
+    avg_sal_cte a 
 inner join 
-    hr_department b
+    hr_department b 
 on 
     a.dept_id = b.dept_id
-group by 
-    a.dept_id, b.dept_name_en
 order by 
-    avg_sal desc
-    
+    a.avg_sal desc
