ea0a9ec16f488f5b5ad7e2ed0dde2a86af6bb339
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-12-02 21:02:52 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 1, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/3/299307. 대장균의 크기에 따라 분류하기 1/대장균의 크기에 따라 분류하기 1.sql
+++ MySQL/프로그래머스/3/299307. 대장균의 크기에 따라 분류하기 1/대장균의 크기에 따라 분류하기 1.sql
@@ -1,8 +1,8 @@
 select
     id,
     case when size_of_colony > 1000 then 'HIGH'
-    when size_of_colony > 100 then 'MEDIUM'
-    else 'LOW' end as size
+        when size_of_colony > 100 then 'MEDIUM'
+        else 'LOW' end as size
 from 
     ecoli_data
 order by 
