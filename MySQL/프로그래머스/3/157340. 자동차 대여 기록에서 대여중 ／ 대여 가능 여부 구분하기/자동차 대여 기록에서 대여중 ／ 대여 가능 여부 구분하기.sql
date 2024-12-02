85c8896ee5df2740001131981f0416ba8737c1cc
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-12-02 12:11:46 +0900

    [level 3] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ MySQL/프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -1,21 +1,18 @@
-with base as (
+with filtered_car_id as (
     select 
-        history_id,
-        car_id,
-        start_date,
-        end_date
-    from
+        distinct 
+        car_id
+    from 
         car_rental_company_rental_history
     where 
         '2022-10-16' between start_date and end_date
 )
-select 
-    distinct 
+select
+    distinct
     car_id,
-    case when car_id not in (
-        select distinct car_id from base
-    ) then '대여 가능' else '대여중' end as availability
+    case when car_id not in (select * from filtered_car_id) then '대여 가능'
+        else '대여중' end as availability
 from 
-    CAR_RENTAL_COMPANY_RENTAL_HISTORY 
-order by 
+    car_rental_company_rental_history
+order by    
     car_id desc
